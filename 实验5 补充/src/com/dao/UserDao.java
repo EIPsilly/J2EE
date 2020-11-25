d73d1e9d38ea5f7de7de238c7318ccac75a24d59
EIPsilly <1020341547@qq.com>
2020-11-25 16:51:09 +0800

    实验5 补充 提高实验 7

--- 实验5 补充/src/com/dao/UserDao.java
+++ 实验5 补充/src/com/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.dao;
 
 import com.mybatis.po.MyUser;
+import com.pojo.SelectUserParam;
 import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +9,7 @@ import java.util.Map;
 @Mapper
 public interface UserDao {
     public MyUser SelectUserById(Integer id);
-    public List<MyUser> selectAllUser(Map<String, Object> map);
+    public List<MyUser> selectAllUser(SelectUserParam su);
     public int addUser(MyUser user);
     public int updateUser(MyUser user);
     public int deleteUser(MyUser user);
--- 实验5 补充/src/com/mybatis/test/MyBatisTest.java
+++ 实验5 补充/src/com/mybatis/test/MyBatisTest.java
@@ -2,6 +2,7 @@ package com.mybatis.test;
 
 import com.dao.UserDao;
 import com.mybatis.po.MyUser;
+import com.pojo.SelectUserParam;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -19,17 +20,11 @@ public class MyBatisTest {
             InputStream config = Resources.getResourceAsStream("mybatis-config.xml");
             SqlSessionFactory ssf = new SqlSessionFactoryBuilder().build(config);
             SqlSession ss = ssf.openSession();
-//            MyUser mu = ss.selectOne("com.mybatis.mapper.UserMapper.selectUserById",1);
-//            System.out.println(mu.getUname() + " " + mu.getUsex());
-//            MyUser addmu = new MyUser();
-//            addmu.setUname("张三");
-//            addmu.setUsex("男");
-//            ss.insert("com.mybatis.mapper.UserMapper.addUser",addmu);
-            Map<String,Object> map = new HashMap<>();
-            map.put("u_name","c");
-            map.put("u_sex","男");
+            SelectUserParam su = new SelectUserParam();
+            su.setU_name("陈");
+            su.setU_sex("男");
             UserDao userDao = ss.getMapper(UserDao.class);
-            List<MyUser> list = userDao.selectAllUser(map);
+            List<MyUser> list = userDao.selectAllUser(su);
             for (MyUser myUser:list){
                 System.out.println("编号:"+myUser.getUid()+"\n姓名:"+myUser.getUname()+"\n性别:"+myUser.getUsex());
             }
