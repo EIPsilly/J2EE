55cf37c3aedc24bf97d4429d8db5918dfd184d33
EIPsilly <1020341547@qq.com>
2020-11-25 13:02:45 +0800

    实验5 提高实验 2 Map 接口传入参数

--- 实验5 补充/src/com/mybatis/test/MyBatisTest.java
+++ 实验5 补充/src/com/mybatis/test/MyBatisTest.java
@@ -8,6 +8,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MyBatisTest {
     public static void main(String[] args){
@@ -15,12 +18,19 @@ public class MyBatisTest {
             InputStream config = Resources.getResourceAsStream("mybatis-config.xml");
             SqlSessionFactory ssf = new SqlSessionFactoryBuilder().build(config);
             SqlSession ss = ssf.openSession();
-            MyUser mu = ss.selectOne("com.mybatis.mapper.UserMapper.selectUserById",1);
-            System.out.println(mu.getUname() + " " + mu.getUsex());
-            MyUser addmu = new MyUser();
-            addmu.setUname("张三");
-            addmu.setUsex("男");
-            ss.insert("com.mybatis.mapper.UserMapper.addUser",addmu);
+//            MyUser mu = ss.selectOne("com.mybatis.mapper.UserMapper.selectUserById",1);
+//            System.out.println(mu.getUname() + " " + mu.getUsex());
+//            MyUser addmu = new MyUser();
+//            addmu.setUname("张三");
+//            addmu.setUsex("男");
+//            ss.insert("com.mybatis.mapper.UserMapper.addUser",addmu);
+            Map<String,Object> map = new HashMap<>();
+            map.put("u_name","a");
+            map.put("u_sex","男");
+            List<MyUser> list = ss.selectList("com.mybatis.mapper.UserMapper.selectAllUser",map);
+            for (MyUser myUser:list){
+                System.out.println("编号:"+myUser.getUid()+"\n姓名:"+myUser.getUname()+"\n性别:"+myUser.getUsex());
+            }
             ss.commit();
             ss.close();
         } catch (IOException e){
